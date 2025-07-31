86093f50a08a5c7b1fe354dc3af3d74ee2069b5c
chuiyugin <1726123207@qq.com>
2025-07-31 15:13:55 +0800

    update

---
+++ sqlx_demo/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql" // 不要忘了导入数据库驱动
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB // 定义全局 db 变量
+
+func initDB() (err error) {
+	dsn := "yugin:1234@tcp(127.0.0.1:3306)/sql_test?charset=utf8mb4&parseTime=True"
+	// 也可以使用MustConnect连接不成功就panic
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("connect DB failed, err:%v\n", err)
+		return
+	}
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(10)
+	return
+}
+
+type user struct {
+	ID   int    `db:"id"`
+	Age  int    `db:"age"`
+	Name string `db:"name"`
+}
+
+// 查询单条数据示例
+func queryRowDemo() {
+	sqlStr := "select id, name, age from user where id=?"
+	var u user
+	err := db.Get(&u, sqlStr, 1) // 直接传结构体指针进去
+	if err != nil {
+		fmt.Printf("get failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.ID, u.Name, u.Age)
+}
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	var users []user // 构造user结构体的切片
+	err := db.Select(&users, sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("users:%#v\n", users)
+}
+
+// 插入数据
+func insertRowDemo() {
+	sqlStr := "insert into user(name, age) values (?,?)"
+	ret, err := db.Exec(sqlStr, "nikola", 19)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入数据的id
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
+}
+
+// 更新数据
+func updateRowDemo() {
+	sqlStr := "update user set age=? where id = ?"
+	ret, err := db.Exec(sqlStr, 31, 6)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success, affected rows:%d\n", n)
+}
+
+// 删除数据
+func deleteRowDemo() {
+	sqlStr := "delete from user where id = ?"
+	ret, err := db.Exec(sqlStr, 6)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
+}
+
+func main() {
+	if err := initDB(); err != nil {
+		fmt.Printf("init db failed!, err: %#v\n", err)
+	}
+	fmt.Println("init db sucess!")
+	// queryRowDemo() // 查询单条数据
+	// insertRowDemo() // 插入数据
+	// updateRowDemo()     // 更新数据
+	deleteRowDemo()     // 删除数据
+	queryMultiRowDemo() // 查询多条数据
+
+}
