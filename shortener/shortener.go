00a962d58fa67bce6ecd490a9f257a2e6cf11015
chuiyugin <1726123207@qq.com>
2025-09-01 01:04:33 +0800

    update

---
+++ shortener/shortener.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"shortener/internal/config"
+	"shortener/internal/handler"
+	"shortener/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/rest"
+)
+
+var configFile = flag.String("f", "etc/shortener-api.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+
+	// 验证配置文件是否加载成功
+	fmt.Printf("load conf: %#v\n", c)
+
+	server := rest.MustNewServer(c.RestConf)
+	defer server.Stop()
+
+	ctx := svc.NewServiceContext(c)
+	handler.RegisterHandlers(server, ctx)
+
+	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
+	server.Start()
+}
