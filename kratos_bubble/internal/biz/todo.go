98d804a1ce86ee880de2fd06d45c281b4ae92b9a
chuiyugin <1726123207@qq.com>
2025-09-04 01:03:26 +0800

    update

---
+++ kratos_bubble/internal/biz/todo.go
@@ -0,0 +1,47 @@
+package biz
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+// var (
+// 	// ErrUserNotFound is user not found.
+// 	ErrUserNotFound = errors.NotFound(v1.ErrorReason_USER_NOT_FOUND.String(), "user not found")
+// )
+
+// Todo is a Todo model.
+type Todo struct {
+	ID     int64
+	Title  string
+	Status bool
+}
+
+// TodoRepo is a Todo repo.
+// biz 层对数据操作层提出了以下接口要求，不管实际存储的是Mysql还是Redis
+type TodoRepo interface {
+	Save(context.Context, *Todo) (*Todo, error)
+	Update(context.Context, *Todo) error
+	Delete(context.Context, int64) error
+	FindByID(context.Context, int64) (*Todo, error)
+	ListAll(context.Context) ([]*Todo, error)
+}
+
+// TodoUsecase is a Todo usecase.
+type TodoUsecase struct {
+	repo TodoRepo
+	log  *log.Helper
+}
+
+// NewTodoUsecase new a Todo usecase.
+func NewTodoUsecase(repo TodoRepo, logger log.Logger) *TodoUsecase {
+	return &TodoUsecase{repo: repo, log: log.NewHelper(logger)}
+}
+
+// CreateTodo creates a Todo, and returns the new Todo.
+// 对外提供的业务函数
+func (uc *TodoUsecase) CreateTodo(ctx context.Context, t *Todo) (*Todo, error) {
+	uc.log.WithContext(ctx).Infof("CreateTodo: %#v", t)
+	return uc.repo.Save(ctx, t) // 调用下一层的Save方法
+}
