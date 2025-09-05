eb8f32167101adeb7af30fa03ed5a8450700e686
chuiyugin <1726123207@qq.com>
2025-09-05 16:30:08 +0800

    update

--- kratos_bubble/internal/server/http.go
+++ kratos_bubble/internal/server/http.go
@@ -1,20 +1,38 @@
 package server
 
 import (
+	"context"
+	"fmt"
 	v1 "kratos_bubble/api/bubble/v1"
 	"kratos_bubble/internal/conf"
 	"kratos_bubble/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
+
+// 自定义中间件
+func Middleware1() middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			// 逻辑执行之前
+			fmt.Println("自定义中间件执行handler执行之前")
+			defer func() {
+				fmt.Println("自定义中间件执行handler执行之后")
+			}()
+			return handler(ctx, req) // 执行目标handler
+		}
+	}
+}
 
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, todo *service.TodoService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			Middleware1(), // 注册上自定义中间件
 		),
 	}
 	if c.Http.Network != "" {
